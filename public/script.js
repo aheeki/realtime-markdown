602e23985195e07fd8e36ee4e9aac43c883f1d36
Andrew Heekin <andrewheekin@gmail.com>
2015-08-27 01:26:08 -0400

    multiple markdown page support

--- public/script.js
+++ public/script.js
@@ -5,6 +5,29 @@ window.onload = function() {
     var pad = document.getElementById('pad');
     var markdownArea = document.getElementById('markdown');
 
+    // make the tab act like a tab
+    pad.addEventListener('keydown',function(e) {
+        if(e.keyCode === 9) { // tab was pressed
+            // get caret position/selection
+            var start = this.selectionStart;
+            var end = this.selectionEnd;
+
+            var target = e.target;
+            var value = target.value;
+
+            // set textarea value to: text before caret + tab + text after caret
+            target.value = value.substring(0, start)
+                + "\t"
+                + value.substring(end);
+
+            // put caret at right position again (add one for the tab)
+            this.selectionStart = this.selectionEnd = start + 1;
+
+            // prevent the focus lose
+            e.preventDefault();
+        }
+    });
+
     var previousMarkdownValue;
 
     var convertTextAreaToMarkdown = function(){
@@ -27,10 +50,20 @@ window.onload = function() {
         }
     }, 1000);
 
+    // convert textarea on input change
     pad.addEventListener('input', convertTextAreaToMarkdown);
 
-    sharejs.open(document.location.pathname, 'text', function(error, doc) {
-        doc.attach_textarea(pad);
-        convertTextAreaToMarkdown();
-    });
+    // ignore if on home page
+    if(document.location.pathname.length > 1){
+        // implement share js
+        var documentName = document.location.pathname.substring(1);
+        sharejs.open(documentName, 'text', function(error, doc) {
+            doc.attach_textarea(pad);
+            convertTextAreaToMarkdown();
+        });
+    }
+
+    // convert on page load
+    convertTextAreaToMarkdown();
+
 };
